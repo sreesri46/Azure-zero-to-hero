75ed7122c33dd9e10ea4bba33730b81d8199a19e
srinivas <prathik.srinu@gmail.com>
2025-11-22 12:12:56 -0500

    test

--- dummy/main.tf
+++ dummy/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                  = "example-machine"
   resource_group_name   = azurerm_resource_group.example.name
   location              = azurerm_resource_group.example.location
-  size                  = "Standard_B2s"
+  size                  = "B2ats_v2"
   admin_username        = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.example.id,
